04ebaac568dca7f9f78784b3026ae303fb56c762
Wajdi Hajji <wh330@cam.ac.uk>
2021-03-27 15:07:01 +0000

    Fixed typo

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `Wajdi Hajji`,
-    subtitle: `Devops Engineer`,
+    subtitle: `DevOps Engineer`,
     description: `A minimal blog starter built with Gatsbyjs. The needed Gatsby files are included.`,
     author: `@niklasmtj`,
   },
